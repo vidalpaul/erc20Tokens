a46272c66329cadd3cc3f4b539260cfb0f72b77f
Paulo Vidal <paulo.vidal@bitfy.app>
2022-07-25 19:29:16 -0300

    add erc20 with fixed supply example with openzeppelin

---
+++ contracts/ERC20FixedSupplyOpenZeppelin.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ERC20FixedSupply is ERC20 {
+    constructor() ERC20("FixedSupply", "FIX") {
+        _mint(msg.sender, 1000);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 18;
+    }
+}
