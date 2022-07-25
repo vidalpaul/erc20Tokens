91fcc085dfa8f7445e64f662a609ea98530458a2
Paulo Vidal <paulo.vidal@bitfy.app>
2022-07-25 20:11:44 -0300

    add erc20 tests

---
+++ test/ERC20MinerRewardOpenZeppelin.ts
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+import hre from 'hardhat';
+
+describe('ERC20MinerRewardOpenZeppelin', () => {
+    it('Should set the correct decimals', async () => {
+        const FixedSupply = await hre.ethers.getContractFactory('ERC20MinerRewardOpenZeppelin');
+        const fixedSupply = await FixedSupply.deploy();
+        // assert that decimals are '18'
+        expect(await fixedSupply.decimals()).to.equal(18);
+        expect(await fixedSupply.name()).to.equal('MinerReward');
+        expect(await fixedSupply.symbol()).to.equal('MRW');
+    });
+});
+
