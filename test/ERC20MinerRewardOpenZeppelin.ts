e3712495488c3185558c2b0e58fc8b603bff0495
Paulo Vidal <u1d4lp@gmail.com>
2022-07-25 21:33:38 -0300

    fix tests

--- test/ERC20MinerRewardOpenZeppelin.ts
+++ test/ERC20MinerRewardOpenZeppelin.ts
@@ -3,12 +3,20 @@ import hre from 'hardhat';
 
 describe('ERC20MinerRewardOpenZeppelin', () => {
     it('Should set the correct decimals', async () => {
-        const FixedSupply = await hre.ethers.getContractFactory('ERC20MinerRewardOpenZeppelin');
-        const fixedSupply = await FixedSupply.deploy();
+        const MinerReward = await hre.ethers.getContractFactory('ERC20MinerRewardOpenZeppelin');
+        const minerReward = await MinerReward.deploy();
         // assert that decimals are '18'
-        expect(await fixedSupply.decimals()).to.equal(18);
-        expect(await fixedSupply.name()).to.equal('MinerReward');
-        expect(await fixedSupply.symbol()).to.equal('MRW');
+        expect(await minerReward.decimals()).to.equal(18);
+        expect(await minerReward.name()).to.equal('MinerReward');
+        expect(await minerReward.symbol()).to.equal('MRW');
     });
+    it('Should transfer 200 MRW', async () => {
+        const MinerReward = await hre.ethers.getContractFactory('ERC20FixedSupplyOpenZeppelin');
+        const minerReward = await MinerReward.deploy();
+        const [addr1, addr2] = await hre.ethers.getSigners();
+        await minerReward.transfer(addr2.address, 200);
+        expect(await minerReward.balanceOf(addr1.address)).to.equal(800);
+        expect(await minerReward.balanceOf(addr2.address)).to.equal(200);
+    })
 });
 
