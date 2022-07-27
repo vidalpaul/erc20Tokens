61ab2a278931bfe3689c6e445bdb8db1a8bfe392
Paulo Vidal <u1d4lp@gmail.com>
2022-07-27 17:24:15 -0300

    add tests for erc777.send()

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "hardhat-erc1820";//for ERC777 deployment
+import "hardhat-erc1820"; //for ERC777 deployment
 
 const config: HardhatUserConfig = {
   solidity: "0.8.15",
--- test/ERC777FixedSupplyOpenZeppelin.ts
+++ test/ERC777FixedSupplyOpenZeppelin.ts
@@ -4,11 +4,18 @@ import hre from 'hardhat';
 describe('ERC777FixedSupplyOpenZeppelin', () => {
     it('Should set the correct contract features', async () => {
         const ERC777 = await hre.ethers.getContractFactory('ERC777FixedSupplyOpenZeppelin');
-        const [addr1, addr2] = await hre.ethers.getSigners();
         const erc777 = await ERC777.deploy(10000, []);
         // assert that decimals are '18'
         expect(await erc777.decimals()).to.equal(18);
         expect(await erc777.name()).to.equal('Gold');
         expect(await erc777.symbol()).to.equal('GLD');
+    });
+    it('Should transfer 200 GLD', async () => {
+        const ERC777 = await hre.ethers.getContractFactory('ERC777FixedSupplyOpenZeppelin');
+        const erc777 = await ERC777.deploy(1000, []);
+        const [addr1, addr2] = await hre.ethers.getSigners();
+        await erc777.send(addr2.address, 200, []);
+        expect(await erc777.balanceOf(addr1.address)).to.equal(800);
+        expect(await erc777.balanceOf(addr2.address)).to.equal(200);
     });
 })
